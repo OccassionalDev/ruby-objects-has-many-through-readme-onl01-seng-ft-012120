4721d90bfb4fd54f2d9aa406e2f49f959db32c66
Erica Schimmel <ericaschimmel01@gmail.com>
2020-02-06 03:41:29 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,15 @@
 class Waiter
-
+  attr_accessor :name, :yrs_experince
+  
+  @@all = []
+  
+  def initialize(name, yrs_experince)
+    @name = name 
+    @yrs_experince = yrs_experince
+    save 
+  end 
+    
+  def save
+    @@all << self 
+  end 
 end
